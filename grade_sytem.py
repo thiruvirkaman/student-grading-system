0293281c75aab07e261fd35903f8bb126ad24656
thiruvirkaman <vikeu99@gmail.com>
2026-07-22 01:43:17 +0530

    removing redundant condition

--- grade_sytem.py
+++ grade_sytem.py
@@ -3,7 +3,7 @@ try:
     mark = float(input("Enter your mark: ")) 
     if mark < 0 or mark > 100:
         print("Invalid mark. ", message)
-    elif mark >= 90 and mark <= 100:
+    elif mark >= 90 :
         print("Grade: A")
     elif mark >= 80 and mark <=89:
         print("Grade: B")
@@ -13,5 +13,5 @@ try:
         print("Grade: D")
     else:
         print("Grade: E")
-except ValueError as e:
+except :
     print("Invalid input. ", message)
